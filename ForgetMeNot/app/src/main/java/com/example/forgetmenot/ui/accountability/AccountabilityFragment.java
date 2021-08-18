3d4776faa429613511384cf7cfc5037223c01fc3
Andy Lin <60021592+andylin2004@users.noreply.github.com>
2021-08-18 15:50:58 -0400

    changing around names

--- ForgetMeNot/app/src/main/java/com/example/forgetmenot/ui/accountability/AccountabilityFragment.java
+++ ForgetMeNot/app/src/main/java/com/example/forgetmenot/ui/accountability/AccountabilityFragment.java
@@ -1,4 +1,4 @@
-package com.example.forgetmenot.ui.notifications;
+package com.example.forgetmenot.ui.accountability;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -14,17 +14,17 @@ import androidx.lifecycle.ViewModelProvider;
 
 import com.example.forgetmenot.R;
 
-public class NotificationsFragment extends Fragment {
+public class AccountabilityFragment extends Fragment {
 
-    private NotificationsViewModel notificationsViewModel;
+    private AccountabilityViewModel accountabilityViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        notificationsViewModel =
-                new ViewModelProvider(this).get(NotificationsViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_notifications, container, false);
-        final TextView textView = root.findViewById(R.id.text_notifications);
-        notificationsViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+        accountabilityViewModel =
+                new ViewModelProvider(this).get(AccountabilityViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_accountability, container, false);
+        final TextView textView = root.findViewById(R.id.text_dashboard);
+        accountabilityViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
--- ForgetMeNot/app/src/main/java/com/example/forgetmenot/ui/accountability/AccountabilityViewModel.java
+++ ForgetMeNot/app/src/main/java/com/example/forgetmenot/ui/accountability/AccountabilityViewModel.java
@@ -1,14 +1,14 @@
-package com.example.forgetmenot.ui.dashboard;
+package com.example.forgetmenot.ui.accountability;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class DashboardViewModel extends ViewModel {
+public class AccountabilityViewModel extends ViewModel {
 
     private MutableLiveData<String> mText;
 
-    public DashboardViewModel() {
+    public AccountabilityViewModel() {
         mText = new MutableLiveData<>();
         mText.setValue("This is dashboard fragment");
     }
